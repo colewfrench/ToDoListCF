5c2d550c9aec7c93f8af25ee719a8c274bb8a3e0
colewfrench <colew.french@gmail.com>
2017-02-27 14:54:46 -0800

    test

--- app/src/main/java/com/example/colefrench/todolist/MainActivity.java
+++ app/src/main/java/com/example/colefrench/todolist/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends Activity implements Constants {
     private Button addNewEntryButton;
     private Button clearCheckedEntriesButton;
 
-    private int testInt;
-
     private TextView currentDateHeader;
 
     @Override
